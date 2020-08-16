6ff009f3da22294905bbf01ffb8d531a4cb0eeab
jgn1234 <60579482+jgn1234@users.noreply.github.com>
2020-08-16 18:57:08 -0400

    Refactor

--- BerlinClock.fs
+++ BerlinClock.fs
@@ -12,10 +12,10 @@ type Time = {
   Second: Second
 }
 
-type RowGenerator = {
+type ClockRowGenerator = {
   RowList: int List
   RowAction: int -> int -> string
-  TimeValue: TimeValue
+  RowTimeType: TimeValue
 }
 
 let modulo x y = y % x
@@ -25,31 +25,32 @@ let onOrOff trueValue falseValue measure elem =
   | true -> trueValue 
   | false -> falseValue
 
-let rows = [
+
+let clockRows = [
   {
     RowList = [1]; 
     RowAction = modulo 2 >> onOrOff "O" "Y"; 
-    TimeValue = Second
+    RowTimeType = Second
   };
   {
     RowList = [5;10;15;20]; 
     RowAction = onOrOff "R" "O"; 
-    TimeValue = Hour
+    RowTimeType = Hour
   };
   {
     RowList = [1;2;3;4]; 
     RowAction = modulo 5 >> onOrOff "R" "O"; 
-    TimeValue = Hour
+    RowTimeType = Hour
   };
   {
     RowList = [5;10;15;20;25;30;35;40;45;50;55]; 
     RowAction = onOrOff "Y" "O"; 
-    TimeValue = Minute
+    RowTimeType = Minute
   };
   {
     RowList = [1;2;3;4]; 
     RowAction = modulo 5 >> onOrOff "Y" "O"; 
-    TimeValue = Minute
+    RowTimeType = Minute
   };
 ]
 
@@ -64,16 +65,16 @@ let extractTimeValue inputTime timeValue =
     | Hour ->   inputTime.Hour
 
 let rowResults inputTime = 
-  let mapAction (elem: RowGenerator) = 
-    elem.TimeValue |> extractTimeValue inputTime |> elem.RowAction |> rowLights elem.RowList
-  rows |> List.map mapAction
+  let mapAction (elem: ClockRowGenerator) = 
+    elem.RowTimeType |> extractTimeValue inputTime |> elem.RowAction |> rowLights elem.RowList
+  clockRows |> List.map mapAction
 
 let arrayToTime (timeArray: string[]) = {
   Hour = int timeArray.[0]; 
   Minute = int timeArray.[1]; 
   Second = int timeArray.[2]
   }
-  
+
 let timeStringToArray (timeStr: string) = timeStr.Split ':'
 let timeStringToTime = timeStringToArray >> arrayToTime
 
